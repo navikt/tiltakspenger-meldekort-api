b471850aa54bda2918f90de17d3e012d01943819
Richard Martinsen <github@martinsen.org>
2024-01-18 10:32:44 +0100

    R - Lag egen variabel for databaseconf
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>
    Co-authored-by: sneha-d-desai <sneha.desai@nav.no>

--- src/main/kotlin/no/nav/tiltakspenger/meldekort/api/db/DataSource.kt
+++ src/main/kotlin/no/nav/tiltakspenger/meldekort/api/db/DataSource.kt
@@ -7,30 +7,23 @@ import no.nav.tiltakspenger.meldekort.api.Configuration.database
 private val LOG = KotlinLogging.logger {}
 
 object DataSource {
+    private val config = database()
     private const val MAX_POOLS = 3
-
-//    const val DB_USERNAME_KEY = "DB_USERNAME"
-//    const val DB_PASSWORD_KEY = "DB_PASSWORD"
-//    const val DB_DATABASE_KEY = "DB_DATABASE"
-//    const val DB_HOST_KEY = "DB_HOST"
-//    const val DB_PORT_KEY = "DB_PORT"
     const val FAIL_TIMEOUT = 5000
-
-//    private fun getProperty(key: String) = System.getenv(key) ?: System.getProperty(key)
 
     private fun init(): HikariDataSource {
         LOG.info {
-            "Kobler til postgress '${database().brukernavn}:XXX@" +
-                "${database().host}:${database().port}/${database().database}"
+            "Kobler til postgress '${config.brukernavn}:XXX@" +
+                "${config.host}:${config.port}/${config.database}"
         }
 
         return HikariDataSource().apply {
             dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
-            addDataSourceProperty("serverName", database().host)
-            addDataSourceProperty("portNumber", database().port)
-            addDataSourceProperty("databaseName", database().database)
-            addDataSourceProperty("user", database().brukernavn)
-            addDataSourceProperty("password", database().passord)
+            addDataSourceProperty("serverName", config.host)
+            addDataSourceProperty("portNumber", config.port)
+            addDataSourceProperty("databaseName", config.database)
+            addDataSourceProperty("user", config.brukernavn)
+            addDataSourceProperty("password", config.passord)
             initializationFailTimeout = FAIL_TIMEOUT.toLong()
             maximumPoolSize = MAX_POOLS
         }
