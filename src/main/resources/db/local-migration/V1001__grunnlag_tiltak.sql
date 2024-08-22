2c2079b8848ba3d55ca84f48ac78375e47de4de0
Richard Martinsen <github@martinsen.org>
2024-08-22 11:46:06 +0200

    F - Legg til nytt grunnlag og meldekort i lokal migrering

--- src/main/resources/db/local-migration/V1001__grunnlag_tiltak.sql
+++ src/main/resources/db/local-migration/V1001__grunnlag_tiltak.sql
@@ -1,4 +1,5 @@
 INSERT INTO public.grunnlag_tiltak (id,grunnlag_id,fom,tom,typekode,antall_dager_pr_uke) VALUES
     ('9d6eadf8-4d15-413b-9c1f-d4941cbe13f1','c816490e-c031-4d7c-b529-033452930f71','2023-10-01','2024-03-01','GRUFAGYRKE',5),
     ('d7009615-6bd5-4006-be2a-ff6149ba70f6','f0760786-e7ef-4843-a028-6665e8d5eaef','2024-01-01','2024-06-30','ENKFAGYRKE',5),
-    ('ef3d2af8-ab6a-4e87-b79b-2bce8599a411','c55596cf-8373-4b01-aac6-8d0f46794808','2023-10-01','2025-03-28','ARBTREN',2);
+    ('ef3d2af8-ab6a-4e87-b79b-2bce8599a411','c55596cf-8373-4b01-aac6-8d0f46794808','2023-10-01','2025-03-28','ARBTREN',2),
+    ('845df9cd-bd35-4adf-9ef6-8edd658f383a', 'ef63c687-32c9-4ec5-9472-bbd05088e43d', '2024-08-01', '2024-12-31', 'GRUPPE_AMO', 5);
