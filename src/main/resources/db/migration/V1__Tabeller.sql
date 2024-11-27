7d9d6351979776120f202988cef2ab8d85bd364c
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-11-27 15:59:09 +0100

    Legg til migrasjonskript som fjerner rammevedtakId og forrigeMeldekortId og legger til meldeperiodeId

--- src/main/resources/db/migration/V1__Tabeller.sql
+++ src/main/resources/db/migration/V1__Tabeller.sql
@@ -1,12 +1,13 @@
 create table meldekort
 (
-    id                  varchar primary key,
-    sak_id              varchar not null,
-    fnr                 varchar not null,
-    fra_og_med          date    not null,
-    til_og_med          date    not null,
-    meldeperiode_id     varchar not null,
-    meldekortdager      jsonb   not null,
-    status              varchar not null,
-    iverksatt_tidspunkt timestamptz null
+    id                   varchar primary key,
+    sak_id               varchar not null,
+    rammevedtak_id       varchar not null,
+    fnr                  varchar not null,
+    forrige_meldekort_id varchar null references meldekort(id),
+    fra_og_med           date    not null,
+    til_og_med           date    not null,
+    meldekortdager       jsonb   not null,
+    status               varchar not null,
+    iverksatt_tidspunkt  timestamptz null
 );
