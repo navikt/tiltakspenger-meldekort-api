c8eff3486bcee564b1ab86fb22037c1d4c133a25
John Andre Hestad <john.andre.hestad@nav.no>
2025-02-24 20:52:02 +0100

    Migrer hendelse_% til meldeperiode_%

---
+++ src/main/resources/db/migration/V9__rename_meldeperiode_id_value.sql
@@ -0,0 +1,14 @@
+ALTER TABLE meldekort_bruker DROP CONSTRAINT meldekort_bruker_meldeperiode_id_fkey;
+
+UPDATE meldeperiode
+  SET id = REPLACE(id, 'hendelse_', 'meldeperiode_')
+  WHERE id LIKE 'hendelse_%';
+
+UPDATE meldekort_bruker
+  SET meldeperiode_id = REPLACE(meldeperiode_id, 'hendelse_', 'meldeperiode_')
+  WHERE meldeperiode_id LIKE 'hendelse_%';
+
+ALTER TABLE meldekort_bruker
+  ADD CONSTRAINT meldekort_bruker_meldeperiode_id_fkey
+  FOREIGN KEY (meldeperiode_id)
+  REFERENCES meldeperiode(id);
