da1d9934cdd48587d93efa899d1b48c01baa2753
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-07-29 15:30:55 +0200

    legger til asserts for korrigering test

--- src/test/kotlin/no/nav/tiltakspenger/routes/requests/KorrigerMeldekort.kt
+++ src/test/kotlin/no/nav/tiltakspenger/routes/requests/KorrigerMeldekort.kt
@@ -5,12 +5,14 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.ApplicationTestBuilder
+import no.nav.tiltakspenger.libs.json.deserialize
+import no.nav.tiltakspenger.meldekort.domene.MeldekortTilBrukerDTO
 import no.nav.tiltakspenger.routes.defaultRequest
 
 suspend fun ApplicationTestBuilder.korrigerMeldekort(
     meldekortId: String,
     dager: String,
-): String {
+): Pair<MeldekortTilBrukerDTO, String> {
     this.defaultRequest(
         method = io.ktor.http.HttpMethod.Patch,
         uri = "/brukerfrontend/$meldekortId/korriger",
@@ -18,7 +20,7 @@ suspend fun ApplicationTestBuilder.korrigerMeldekort(
         setBody(dager)
     }.let { response ->
         response.status shouldBe HttpStatusCode.OK
-
-        return response.bodyAsText()
+        val body = response.bodyAsText()
+        return deserialize<MeldekortTilBrukerDTO>(body) to body
     }
 }
