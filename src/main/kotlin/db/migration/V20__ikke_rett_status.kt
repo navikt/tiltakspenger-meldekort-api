adba8096d8ef49f1736c10576c9da4916c5900dd
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-08-14 15:22:58 +0200

    fjern innhold fra en migrering som er kjørt

--- src/main/kotlin/db/migration/V20__ikke_rett_status.kt
+++ src/main/kotlin/db/migration/V20__ikke_rett_status.kt
@@ -2,107 +2,11 @@
 
 package db.migration
 
-import io.github.oshai.kotlinlogging.KotlinLogging
-import kotliquery.Row
-import kotliquery.Session
-import no.nav.tiltakspenger.libs.common.MeldekortId
-import no.nav.tiltakspenger.libs.meldekort.MeldeperiodeId
-import no.nav.tiltakspenger.libs.persistering.infrastruktur.PostgresSessionFactory
-import no.nav.tiltakspenger.libs.persistering.infrastruktur.SessionCounter
-import no.nav.tiltakspenger.libs.persistering.infrastruktur.sqlQuery
-import no.nav.tiltakspenger.meldekort.domene.Meldekort
-import no.nav.tiltakspenger.meldekort.domene.MeldekortDag
-import no.nav.tiltakspenger.meldekort.domene.MeldekortDagStatus
-import no.nav.tiltakspenger.meldekort.domene.VarselId
-import no.nav.tiltakspenger.meldekort.domene.journalføring.JournalpostId
-import no.nav.tiltakspenger.meldekort.repository.MeldeperiodePostgresRepo
-import no.nav.tiltakspenger.meldekort.repository.tilMeldekortDagDbJson
-import no.nav.tiltakspenger.meldekort.repository.toMeldekortDager
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 
 internal class V20__ikke_rett_status : BaseJavaMigration() {
     override fun migrate(context: Context) {
-        val logger = KotlinLogging.logger {}
-        val dataSource = context.configuration.dataSource
-        val sessionFactory = PostgresSessionFactory(dataSource, SessionCounter(logger))
-
-        //DENNE KAN VÆRE NULL SIDEN FROM-ROW KAN RETURNERE NULL - IKKE STOL PÅ KOTLIN
-        val alleMeldekortBruker: List<Meldekort> = sessionFactory.withSession { session ->
-            session.run(
-                sqlQuery("""select * from meldekort_bruker""")
-                    .map { row -> fromRow(row, session) }.asList
-            )
-        }.mapNotNull { it }
-
-        alleMeldekortBruker.forEach { meldekort ->
-            val meldeperiode = meldekort.meldeperiode
-
-            //denne saken trenger ikke migrering
-            if (meldekort.sakId.toString().contains("01JCN9")) {
-                return@forEach
-            }
-
-            val dager = meldeperiode.girRett.toList().zip(meldekort.dager).map { (girRett, meldekortDag) ->
-                val harRettPåDenneDagen = girRett.second
-
-                if (harRettPåDenneDagen) {
-                    //hvis bruker har rett, så skal dagen være utfylt med noe fra før (??)
-                    MeldekortDag(
-                        dag = meldekortDag.dag,
-                        status = meldekortDag.status,
-                    )
-                } else {
-                    //har ikke rett - skal sette status til IKKE_RETT_TIL_TILTAKSPENGER (denne skal være IKKE_BESVART)
-                    MeldekortDag(
-                        dag = meldekortDag.dag,
-                        status = MeldekortDagStatus.IKKE_RETT_TIL_TILTAKSPENGER,
-                    )
-                }
-            }
-
-            sessionFactory.withSession { session ->
-                session.run(
-                    sqlQuery(
-                        """
-                            update meldekort_bruker
-                            set dager = to_jsonb(:dager::jsonb)
-                            where id = :id
-                        """.trimIndent(),
-                        "dager" to dager.tilMeldekortDagDbJson(),
-                        "id" to meldekort.id.toString(),
-                    ).asUpdate
-                )
-            }
-        }
+        //allerede kjørt - trenger ikke innholdet
     }
-}
-
-private fun fromRow(
-    row: Row,
-    session: Session,
-): Meldekort? {
-    val id = MeldekortId.fromString(row.string("id"))
-    val meldeperiodeId = row.string("meldeperiode_id")
-
-    val meldeperiode = MeldeperiodePostgresRepo.hentForId(MeldeperiodeId.fromString(meldeperiodeId), session)
-
-    //denne saken trenger ikke migrering
-    if (meldeperiode?.sakId.toString().contains("01JCN9")) {
-        return null
-    }
-
-    requireNotNull(meldeperiode) { "Fant ingen meldeperiode for $id" }
-
-    return Meldekort(
-        id = id,
-        meldeperiode = meldeperiode,
-        mottatt = row.localDateTimeOrNull("mottatt"),
-        deaktivert = row.localDateTimeOrNull("deaktivert"),
-        dager = row.string("dager").toMeldekortDager(),
-        journalpostId = row.stringOrNull("journalpost_id")?.let { JournalpostId(it) },
-        journalføringstidspunkt = row.localDateTimeOrNull("journalføringstidspunkt"),
-        varselId = row.stringOrNull("varsel_id")?.let { VarselId(it) },
-        erVarselInaktivert = row.boolean("varsel_inaktivert"),
-    )
 }
