97cb70bb2a7765021fc7763fc4971bbe22a94a1f
Henrik Solum <henrik.solum@nav.no>
2025-11-14 13:51:37 +0100

    1720 kjede_id er ikke unik for seg selv, må derfor gruppere på sak også
    fordi kjede_id er jo bare tilOgMed / fraOgMed datoer, så vil alle meldekort med samme perioder få de samme kjede_id-ene

--- src/main/resources/db/migration/V24__meldekort_korrigering.sql
+++ src/main/resources/db/migration/V24__meldekort_korrigering.sql
@@ -3,7 +3,7 @@ ALTER TABLE meldekort_bruker
 
 WITH meldekort_i_kjede AS (
     SELECT mb.id,
-           row_number() OVER (PARTITION BY mp.kjede_id ORDER BY mb.mottatt) AS index
+           row_number() OVER (PARTITION BY mp.sak_id, mp.kjede_id ORDER BY mb.mottatt, mb.id) AS indeks
     FROM meldekort_bruker mb
     JOIN meldeperiode mp ON mp.id = mb.meldeperiode_id
     WHERE mb.mottatt IS NOT NULL
@@ -12,4 +12,4 @@ UPDATE meldekort_bruker mb
 SET korrigering = true
 FROM meldekort_i_kjede meldekort
 WHERE mb.id = meldekort.id
-  AND meldekort.index > 1;
+  AND meldekort.indeks > 1;
