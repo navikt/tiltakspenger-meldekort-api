bce20bd1f28741425e4f3d85fa7faa7f491b9299
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-11-21 16:41:16 +0100

    Oppdaterer beregning av kan_fylles_ut_fra_og_med til å være to dager før til_og_med

--- src/main/resources/db/migration/V24__meldeperiode_kanFyllesUtFraOgMed.sql
+++ src/main/resources/db/migration/V24__meldeperiode_kanFyllesUtFraOgMed.sql
@@ -2,7 +2,7 @@ ALTER TABLE meldeperiode
     ADD COLUMN kan_fylles_ut_fra_og_med TIMESTAMP;
 
 UPDATE meldeperiode
-SET kan_fylles_ut_fra_og_med = (til_og_med || ' 00:00:00')::TIMESTAMP;
+SET kan_fylles_ut_fra_og_med = (SELECT til_og_med::date - 2 || ' 00:00:00')::TIMESTAMP;
 
 ALTER TABLE meldeperiode
     ALTER COLUMN kan_fylles_ut_fra_og_med SET NOT NULL;
