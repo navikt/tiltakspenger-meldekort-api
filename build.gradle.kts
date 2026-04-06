2bd8afe34238f63263990576e9288171c92e7f64
John Andre Hestad <john.andre.hestad@nav.no>
2026-04-06 14:51:21 +0200

    Bump libs til 0.0.737

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val mainClassFile = "no.nav.tiltakspenger.meldekort.ApplicationKt"
 
 val ktorVersion = "3.4.2"
 val mockkVersion = "1.14.9"
-val felleslibVersion = "0.0.724"
+val felleslibVersion = "0.0.737"
 val kotestVersion = "6.1.10"
 val kotlinxCoroutinesVersion = "1.10.2"
 val tmsVarselBuilderVersion = "2.2.0"
