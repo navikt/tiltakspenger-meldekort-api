d0d0036ca83d42bae4e2f771ce1de950229737fa
Anders Nomerstad <anders@nomerstad.com>
2026-07-30 09:40:23 +0200

    Oppdaterer libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val mainClassFile = "no.nav.tiltakspenger.meldekort.infra.ApplicationKt"
 
 val ktorVersion = "3.4.3"
 val mockkVersion = "1.14.11"
-val felleslibVersion = "0.0.20260729145801"
+val felleslibVersion = "0.0.20260730072901"
 val kotestVersion = "6.2.3"
 val kotlinxCoroutinesVersion = "1.11.0"
 val tmsVarselBuilderVersion = "2.2.0"
